c1fe4672c4f942e32c75beec9881039fd1fdfd6c
Daniel Nachun <daniel.nachun@gmail.com>
2024-09-20 11:53:37 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,7 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 go build -trimpath -buildmode=pie -o=${PREFIX}/bin/${PKG_NAME} -ldflags="-s -w"
 go-licenses save . --save_path=license-files
 
-if [[ ${build_platform} == ${taret_platform} ]]; then
+if [[ ${build_platform} == ${target_platform} ]]; then
     mkdir -p ${PREFIX}/etc/bash_completion.d
     mkdir -p ${PREFIX}/fish/vendor_completions.d
     mkdir -p ${PREFIX}/zsh/site-functions
